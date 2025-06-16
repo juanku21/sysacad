0acdf56c734dfffb6b584b01ece107ccccd408b5
Tincho <martinmercado2004@gmail.com>
2025-06-15 22:05:52 -0300

    Repositorio y Servicio Calificación Realizado

--- src/services/qualification.service.ts
+++ src/services/qualification.service.ts
@@ -1,25 +1,73 @@
 
+import { Prisma, Qualification } from "@prisma/client"
+import { QualificationRepository } from "../repositories/qualification.repository"
 
+
+const repository = new QualificationRepository()
 
 export class QualificationService {
 
-    public static async get() {
-
+    public static async get() : Promise<Qualification[]> {
+        try {
+            const result = await repository.get()
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible obtener la lista de calificaciones: ${error}`)
+        }
     }
 
-    public static async getById() {
-
+    public static async getById(id : number) : Promise<Qualification | null> {
+        try {
+            const result = await repository.getById(id)
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible obtener la calificación solicitada: ${error}`)
+        }
     }
 
-    public static async create() {
-
+    public static async create(qualification : Prisma.QualificationCreateInput) : Promise<Qualification> {
+        try {
+            const result = await repository.create(qualification)
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible crear una nueva calificación: ${error}`)
+        }
     }
 
-    public static async update()  {
+    public static async update(id : number, qualification : Prisma.QualificationCreateInput) : Promise<Qualification>  {
+        try {
+            const qualificationExists = await repository.getById(id)
 
+            if (qualificationExists === null) {
+                throw new Error("La calificación que desea actualizar no existe")    
+            }
+
+            const result = await repository.update(id, qualification)
+            return result
+
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible actualizar la calificación solicitada: ${error}`)
+        }
     }
 
-    public static async delete() {
+    public static async delete(id : number) : Promise<Qualification> {
+        try {
+            const qualificationExists = await repository.getById(id)
+
+            if (qualificationExists === null) {
+                throw new Error("La calificación que desea eliminar no existe")    
+            }
+
+            const result = await repository.delete(id)
+            return result
+        } 
+        catch (error : any) {
+            throw new Error(`No fue posible eliminar la calificación solicitada: ${error}`)
+        }
 
     }
 
