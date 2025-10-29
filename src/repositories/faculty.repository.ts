b09f49d1c85edb8e1ddddc1f9e27156638aa3719
Juan Cruz Diaz <djuancruz21@gmail.com>
2025-10-29 12:05:51 -0300

    Filtrado y paginado terminado para el endpoint de facultad

--- src/repositories/faculty.repository.ts
+++ src/repositories/faculty.repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, Faculty} from "@prisma/client"
 import { FacultyWithRelations } from "../types"
 import { BaseRepository } from "./base.repository"
+import { PrismaFilterTransformer } from "../utils/filterAdapter"
 import prisma from "../config/client"
 
 export class FacultyRepository extends BaseRepository 
@@ -28,6 +29,29 @@ export class FacultyRepository extends BaseRepository
         catch (error : any) {
             throw new Error(`Error al leer la base de datos`)
         }
-    }   
+    }
+
+    public async getFiltered(filter : string, pageNumber : number = 1, pageSize : number = 100) : Promise<Faculty[]> {
+        try {
+
+            const skipAmount = (pageNumber - 1) * pageSize
+            
+            const prismaFilter = filter ? PrismaFilterTransformer.toPrismaWhere(filter) : {}
+
+            console.log(prismaFilter)
+
+            const result = await prisma.faculty.findMany({
+                where: prismaFilter,
+                skip: skipAmount,
+                take: pageSize
+            })
+
+            return result
+
+        } 
+        catch (error : any) {
+            throw new Error(`Error al leer la base de datos: ${error}`)
+        }
+    } 
     
 }
