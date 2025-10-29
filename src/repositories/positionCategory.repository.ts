458df1efb602a48ab3453b1de57a5a328f449d09
Pablo <pmaturano3@gmail.com>
2025-10-29 14:17:09 -0300

    Realizado el filtrado y paginado para el modulo position category

--- src/repositories/positionCategory.repository.ts
+++ src/repositories/positionCategory.repository.ts
@@ -2,6 +2,7 @@
 import { Prisma, PositionCategory} from "@prisma/client"
 import { PositionCategoryWithRelations } from "../types"
 import { BaseRepository } from "./base.repository"
+import { PrismaFilterTransformer } from "../utils/filterAdapter"
 import prisma from "../config/client"
 
 export class PositionCategoryRepository extends BaseRepository 
@@ -27,6 +28,29 @@ export class PositionCategoryRepository extends BaseRepository
         catch (error : any) {
             throw new Error(`Error al leer la base de datos`)
         }
-    }   
+    }
+
+    public async getFiltered(filter : string, pageNumber : number = 1, pageSize : number = 100) : Promise<PositionCategory[]> {
+        try {
+
+            const skipAmount = (pageNumber - 1) * pageSize
+            
+            const prismaFilter = filter ? PrismaFilterTransformer.toPrismaWhere(filter) : {}
+
+            console.log(prismaFilter)
+
+            const result = await prisma.positionCategory.findMany({
+                where: prismaFilter,
+                skip: skipAmount,
+                take: pageSize
+            })
+
+            return result
+
+        } 
+        catch (error : any) {
+            throw new Error(`Error al leer la base de datos: ${error}`)
+        }
+    } 
     
 }
